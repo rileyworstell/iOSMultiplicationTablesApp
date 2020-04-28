928d243dfdb8990d199fea304e5683af86fff86f
Riley Worstell <rcwors02@gmail.com>
2020-04-27 22:44:45 -0400

    first commit

--- MultiplicationTables/ViewController.swift
+++ MultiplicationTables/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var answer = 144
+    @IBOutlet weak var num1: UILabel!
+    
+    @IBOutlet weak var num2: UILabel!
+    
+    @IBOutlet weak var isWrong: UILabel!
+    @IBOutlet weak var isCorrect: UILabel!
+    @IBAction func answerInput(_ sender: Any) {
+        return
+    }
+    
+    @IBOutlet weak var answerValue: UITextField!
+    
+    @IBAction func submitClicked(_ sender: Any) {
+        print(answerValue.text ?? 0)
+        let product = Int(num1.text!)! * Int(num2.text!)!
+        let intAnswer = Int(answerValue.text!)
+       if product == intAnswer {
+            print("Correct")
+        isWrong.isHidden = true
+        isCorrect.isHidden = false
+        
+        let num1Val = Int.random(in: 0 ... 12)
+        let num2Val = Int.random(in: 0 ... 12)
+        num1.text = String(num1Val)
+        num2.text = String(num2Val)
+        answerValue.text = ""
+        
+       } else {
+        isWrong.isHidden = false
+        isCorrect.isHidden = true
+        print("Wrong")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+      //  answerValue.delegate = self
+        isCorrect.isHidden = true
+        isWrong.isHidden = true
         // Do any additional setup after loading the view.
     }
+    
 
-
+    
 }
 
